4c9f6ffd2f8223f12ecf829bab2513c1bbcd97bc
Janujah <janujahsivarattinam@gmail.com>
2024-05-29 22:29:05 +0530

    Deploy

--- client/src/Technicians/techAppoinment.js
+++ client/src/Technicians/techAppoinment.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-// import BookingForm from './booking';
-import AppointmentCalendar from '../Components/AppointmentCalendar';
-import Nav from '../Components/technav'
+// import React, { useState } from 'react';
+// // import BookingForm from './booking';
+// import AppointmentCalendar from '../Components/AppointmentCalendar';
+// import Nav from '../Components/technav'
 
-const ParentComponent = () => {
-  const [events, setEvents] = useState([]);
+// const ParentComponent = () => {
+//   // const [events, setEvents] = useState([]);
 
-  const addEventToCalendar = (event) => {
-    setEvents(prevEvents => [...prevEvents, event]);
-  };
+//   // const addEventToCalendar = (event) => {
+//   //   setEvents(prevEvents => [...prevEvents, event]);
+//   // };
 
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Nav/><br/><br/><br/><br/><br/><br/><br></br>
+//   return (
+//     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+//               <Nav/><br/><br/><br/><br/><br/><br/><br></br>
 
-      {/* <BookingForm addEventToCalendar={addEventToCalendar} /> */}
-      <AppointmentCalendar events={events} />
-    </div>
-  );
-};
+//       {/* <BookingForm addEventToCalendar={addEventToCalendar} /> */}
+//       {/* <AppointmentCalendar events={events} /> */}
+//     </div>
+//   );
+// };
 
-export default ParentComponent;
+// export default ParentComponent;
