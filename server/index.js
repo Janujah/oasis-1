964e67774d964da8276c6e0ddfce4cbadb0f4748
Janujah <janujahsivarattinam@gmail.com>
2024-05-29 22:39:35 +0530

    Deploy

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 app.use(
     cors({
-      origin: ["http://localhost:3000","https://oasis-4aui.onrender.com"],
+      origin: ["https://oasis-rqsp.onrender.com/"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
